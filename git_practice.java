eede0fb75b3cb70cce8cec3046c412ca933ba66a
jayanth-chepuri <jayanthnagagandhi_chepuri@srmap.edu.in>
2026-03-06 16:35:09 +0530

    haha

---
+++ git_practice.java
@@ -0,0 +1,11 @@
+public class git_practice {
+    public static void main(String[] args) {
+        System.out.println("Multiplication Table:");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print((i * j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
